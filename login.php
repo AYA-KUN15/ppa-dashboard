d72e64aacc3eebc450d8f7b9875542cc076fc047
AYA-KUN15 <justinebrian15@gmail.com>
2026-02-23 13:28:44 +0800

    v.1.2 Alpha

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - OPMM Dashboard</title>
+    <title>Login - PPA Dashboard</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
